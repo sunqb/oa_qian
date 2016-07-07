105b94d888763c50b82de052f8867d482c7bdf2a
1196239739@qq.com <1196239739@qq.com>
2016-07-07 19:41:15 +0800

    更换kafka类库，使用kafka-python

--- app/mq_c.py
+++ app/mq_c.py
@@ -3,24 +3,18 @@
 #重新设置编码
 import sys
 import logging as log
-from pykafka import KafkaClient
+from kafka import KafkaConsumer
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-log.basicConfig(level=log.DEBUG)
+#log.basicConfig(level=log.DEBUG)
 
-def connectServer(hostlist):
-    client = KafkaClient(hosts=hostlist)
-    return client
-
-def consumer_msg(client):
-    topic = client.topics['oa_qian']
-    consumer = topic.get_simple_consumer()
+def consumer_msg(serverlist):
+    consumer = KafkaConsumer('oa_qian',
+                         group_id='my-group',
+                         bootstrap_servers=[serverlist])
     for message in consumer:
-        if message is not None:
-            print message.offset, message.value
-
+        print message.value
 if __name__ == '__main__':
-    client = connectServer('kafka.sunqb.com:9092')
-    consumer_msg(client)
+    consumer_msg('kafka.sunqb.com:9092')
--- app/mq_s.py
+++ app/mq_s.py
@@ -3,26 +3,19 @@
 #重新设置编码
 import sys
 import logging as log
-from pykafka import KafkaClient
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-log.basicConfig(level=log.DEBUG)
+#log.basicConfig(level=log.DEBUG)
 
-def connectServer(hostlist):
-    client = KafkaClient(hosts=hostlist)
-    return client
-
-def producer_msg(client):
-    topic = client.topics['oa_qian']
-    print topic
-    producer = topic.get_producer()
-    producer.produce('test message ')
-    #for i in range(4):
-    #    producer.produce('test message '+str(i ** 2))
-    #producer.produce(['test message ' + str(i ** 2) for i in range(4)])
+def producer_msg(serverlist):
+    producer = KafkaProducer(bootstrap_servers=[serverlist])
+    producer.send('oa_qian', b'async message')
+    producer.flush()
 
 if __name__ == '__main__':
-    client = connectServer('kafka.sunqb.com:9092')
-    producer_msg(client)
+    producer_msg('kafka.sunqb.com:9092')
